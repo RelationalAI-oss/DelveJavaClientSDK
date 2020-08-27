873b4252379a43b7c1dbf54efb84f776a3319363
Mohammad Dashti <mdashti@gmail.com>
2020-08-27 00:27:59 -0700

    [ClientSDKs/Java] Added a clarification comment
    Co-Authored-By: Nikolas Göbel <me@nikolasgoebel.com>

--- src/extension/java/com/relationalai/client/api/ExtendedApiClient.java
+++ src/extension/java/com/relationalai/client/api/ExtendedApiClient.java
@@ -4,6 +4,7 @@ import com.relationalai.client.ApiClient;
 
 public class ExtendedApiClient extends ApiClient {
     public ApiClient setUserAgent(String userAgent) {
+        // The line below was present in the `ApiClient` and is removed in this override
         // addDefaultHeader("User-Agent", userAgent);
         return this;
     }
