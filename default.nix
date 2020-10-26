bdfb80a8f36c32a7e4bee84ef6597f9a935fd20b
helmi.nour <helmiinour@gmail.com>
2020-10-26 12:46:36 +0100

    nix enabled local tests.

--- default.nix
+++ default.nix
@@ -3,33 +3,27 @@
   delveBinary ? "",
   doCheck ? true
 }:
+
 with pkgs;
 let
   mavenBuild = buildMaven ./project-info.json;
-  build = lib.overrideDerivation mavenBuild.build (oldAttrs: {
-    buildInputs = [ openjdk11 maven ];
-    buildPhase = ''
-      mvn --offline --settings ${mavenBuild.settings} compile
-    '';
-    installPhase = ''
-      mkdir $out
-      mvn --offline --settings ${mavenBuild.settings} package
-      mv target/*.jar $out/
-    '';
-    checkPhase = ''
-    # running delve server
-      mvn --offline --settings ${mavenBuild.settings} test
-    '';
-    doCheck = false;
-  });
-
-in stdenv.mkDerivation rec {
-  name = "java-client-sdk";
-  version = "1.1.0";
-  buildInputs = [ makeWrapper ];
-  src = ./.;
-  buildCommand = ''
-    mkdir -p $out/{lib,bin}
-    cp ${build}/*.jar $out/lib/
+in
+lib.overrideDerivation mavenBuild.build (oldAttrs: {
+  buildInputs = [ openjdk11 maven ];
+  buildPhase = ''
+    mvn --offline --settings ${mavenBuild.settings} compile
   '';
-}
+  installPhase = ''
+    mkdir $out
+    mvn --offline --settings ${mavenBuild.settings} package
+    cp -rv target/*.jar $out/
+  '';
+  checkPhase = ''
+    ${delveBinary}/bin/delve server &
+    PID=$!
+    sleep 15s
+    mvn --offline --settings ${mavenBuild.settings} test || kill -9 $PID && exit -1
+    kill -9 $PID
+  '';
+  doCheck = doCheck;
+})
